945cb8ccbd227bb914ce64d14bb992f73b4a0857
VMLujanJr <vmlujanjr@outlook.com>
2022-04-19 23:48:47 -0500

    created a function with basic markdown syntax

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -12,9 +12,45 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // ..is it array?
+  const [title, description, github, installation, usage, contributing, tests] = data;
+  // ..or is it object?
+  /* const {title, description, github, installation, usage, contributing, tests} = data; */
 
-`;
+  return `#
+    # ${title}
+    
+    ## Description
+
+    [${title}](https://github.com/${github}/${title}) ${description}
+
+    You may visit the deployed [website](https://${github}.github.io/${title}/) to try it out for yourself!
+
+    ## Table of Contents
+
+    - [${title}](#${title})
+      - [Description](#description)
+      - [Table of Contents](#table-of-contents)
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [Contributing](#contributing)
+      - [Tests](#tests)
+
+    ## Installation
+
+    ${installation}
+
+    ## Usage
+
+    ${usage}
+
+    ## Contributing
+
+    ${contributing}
+
+    ## Tests
+    ${tests}
+  `;
 }
 
 module.exports = generateMarkdown;
